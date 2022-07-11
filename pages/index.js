23bfe8e9e768f72bc2a883b2881f4e307733c592
Wright Technology <46791514+wright-technology@users.noreply.github.com>
2022-07-11 18:25:01 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Home = ({ products, bannerData }) => (
   <div>
     <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
     <div className="products-heading">
-      <h2>Straight Outta Madison</h2>
+      <h2>Menu</h2>
       <p>Real People. Real Food. Real Good.</p>
     </div>
 
